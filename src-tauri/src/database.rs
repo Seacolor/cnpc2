88e089c7114277f82da1b2f8e048055413ed1f1b
Kazushi Tominaga <github@seacolorswind.com>
2023-11-27 22:30:32 +0900

    Replace constant from hardcode to sqlite database.

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -6,16 +6,23 @@ use sqlx::{
     Row, SqlitePool,
 };
 
-use crate::{Action, Element, Skill};
+use crate::{
+    Action,
+    Element,
+    Skill,
+    Trait,
+    TraitValue,
+    Item,
+    Text,
+    CaseGroup,
+    Case,
+};
 
 /// このモジュール内の関数の戻り値型
 type DbResult<T> = Result<T, Box<dyn std::error::Error>>;
 
 /// SQLiteのコネクションプールを作成して返す
-pub(crate) async fn create_sqlite_pool(dir_path: &str) -> DbResult<SqlitePool> {
-    // ファイルパス
-    let database_url = format!("{}\\data\\o_skill.db", dir_path);
-
+pub(crate) async fn create_sqlite_pool(database_url: &str) -> DbResult<SqlitePool> {
     // コネクションの設定
     let connection_options = SqliteConnectOptions::from_str(&database_url)?
         // 読み込み専用で開く
@@ -30,7 +37,10 @@ pub(crate) async fn create_sqlite_pool(dir_path: &str) -> DbResult<SqlitePool> {
 }
 
 pub(crate) async fn get_actions(dir_path: &str) -> DbResult<Vec<Action>> {
-    let pool = create_sqlite_pool(dir_path).await?;
+    // ファイルパス
+    let database_url = format!("{}\\data\\o_skill.db", dir_path);
+
+    let pool = create_sqlite_pool(&database_url).await?;
 
     const SQL: &str = "SELECT * FROM o_skill WHERE id >= 400 AND name != '' ORDER BY id ASC";
     let mut rows = sqlx::query(SQL).fetch(&pool);
@@ -47,7 +57,10 @@ pub(crate) async fn get_actions(dir_path: &str) -> DbResult<Vec<Action>> {
 }
 
 pub(crate) async fn get_elements(dir_path: &str) -> DbResult<Vec<Element>> {
-    let pool = create_sqlite_pool(dir_path).await?;
+    // ファイルパス
+    let database_url = format!("{}\\data\\o_skill.db", dir_path);
+
+    let pool = create_sqlite_pool(&database_url).await?;
 
     const SQL: &str = "SELECT * FROM o_skill WHERE id >= 50 AND id < 100 AND name != '' ORDER BY id ASC";
     let mut rows = sqlx::query(SQL).fetch(&pool);
@@ -64,7 +77,10 @@ pub(crate) async fn get_elements(dir_path: &str) -> DbResult<Vec<Element>> {
 }
 
 pub(crate) async fn get_skills(dir_path: &str) -> DbResult<Vec<Skill>> {
-    let pool = create_sqlite_pool(dir_path).await?;
+    // ファイルパス
+    let database_url = format!("{}\\data\\o_skill.db", dir_path);
+
+    let pool = create_sqlite_pool(&database_url).await?;
 
     const SQL: &str = "SELECT * FROM o_skill WHERE id >= 100 AND id < 400 AND name != '' ORDER BY id ASC";
     let mut rows = sqlx::query(SQL).fetch(&pool);
@@ -78,4 +94,98 @@ pub(crate) async fn get_skills(dir_path: &str) -> DbResult<Vec<Skill>> {
     }
 
     Ok(skills)
+}
+
+pub(crate) async fn get_traits(pool: &SqlitePool) -> DbResult<Vec<Trait>> {
+    const SQL: &str = "SELECT * FROM oo_trait ORDER BY id ASC";
+    let mut rows = sqlx::query(SQL).fetch(pool);
+
+    let mut traits = Vec::new();
+    while let Some(row) = rows.try_next().await? {
+        let id: i64 = row.try_get("id")?;
+        let group: i64 = row.try_get("group")?;
+        let text: &str = row.try_get("text")?;
+        let text_e: &str = row.try_get("text_e")?;
+        traits.push(Trait::new(id, group, text, text_e));
+    }
+
+    Ok(traits)
+}
+
+pub(crate) async fn get_trait_values(pool: &SqlitePool) -> DbResult<Vec<TraitValue>> {
+    const SQL: &str = "SELECT * FROM oo_trait_value ORDER BY id,value ASC";
+    let mut rows = sqlx::query(SQL).fetch(pool);
+
+    let mut trait_values = Vec::new();
+    while let Some(row) = rows.try_next().await? {
+        let id: i64 = row.try_get("id")?;
+        let value: i64 = row.try_get("value")?;
+        let text: &str = row.try_get("text")?;
+        let text_e: &str = row.try_get("text_e")?;
+        trait_values.push(TraitValue::new(id, value, text, text_e));
+    }
+
+    Ok(trait_values)
+}
+
+pub(crate) async fn get_items(pool: &SqlitePool) -> DbResult<Vec<Item>> {
+    const SQL: &str = "SELECT * FROM oo_item ORDER BY id ASC";
+    let mut rows = sqlx::query(SQL).fetch(pool);
+
+    let mut items = Vec::new();
+    while let Some(row) = rows.try_next().await? {
+        let id: &str = row.try_get("dbid")?;
+        let reftype: i64 = row.try_get("reftype")?;
+        let reftypeminor: i64 = row.try_get("reftypeminor")?;
+        let name: &str = row.try_get("name")?;
+        let name_e: &str = row.try_get("name_e")?;
+        items.push(Item::new(id, reftype, reftypeminor, name, name_e));
+    }
+
+    Ok(items)
+}
+
+pub(crate) async fn get_texts(pool: &SqlitePool) -> DbResult<Vec<Text>> {
+    const SQL: &str = "SELECT * FROM oo_text ORDER BY id ASC";
+    let mut rows = sqlx::query(SQL).fetch(pool);
+
+    let mut texts = Vec::new();
+    while let Some(row) = rows.try_next().await? {
+        let id: &str = row.try_get("tag")?;
+        let label: &str = row.try_get("label")?;
+        texts.push(Text::new(id, label));
+    }
+
+    Ok(texts)
+}
+
+pub(crate) async fn get_case_groups(pool: &SqlitePool) -> DbResult<Vec<CaseGroup>> {
+    const SQL: &str = "SELECT * FROM oo_case_group ORDER BY id ASC";
+    let mut rows = sqlx::query(SQL).fetch(pool);
+
+    let mut case_groups = Vec::new();
+    while let Some(row) = rows.try_next().await? {
+        let id: &str = row.try_get("expression")?;
+        let label: &str = row.try_get("label")?;
+        case_groups.push(CaseGroup::new(id, label));
+    }
+
+    Ok(case_groups)
+}
+
+pub(crate) async fn get_cases(pool: &SqlitePool) -> DbResult<Vec<Case>> {
+    const SQL: &str = "SELECT * FROM oo_case ORDER BY id ASC";
+    let mut rows = sqlx::query(SQL).fetch(pool);
+
+    let mut cases = Vec::new();
+    while let Some(row) = rows.try_next().await? {
+        let id: &str = row.try_get("expression")?;
+        let value: &str = row.try_get("value")?;
+        let values_size: i64 = row.try_get("values_size")?;
+        let values_type: &str = row.try_get("values_type")?;
+        let label: &str = row.try_get("label")?;
+        cases.push(Case::new(id, value, values_size, values_type, label));
+    }
+
+    Ok(cases)
 }
