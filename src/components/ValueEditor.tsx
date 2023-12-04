03c3253c69d16df70435ce874413dafd0477f9b2
Kazushi Tominaga <github@seacolorswind.com>
2023-12-04 22:57:00 +0900

    Fixed initialize tag's value.

--- src/components/ValueEditor.tsx
+++ src/components/ValueEditor.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { useEffect, FC } from 'react';
 import * as Types from '../Types';
 
 type Props = {
@@ -15,6 +15,18 @@ const ValueEditor: FC<Props> = ({
     const onChange = (value: string) => {
       onValueChange && onValueChange(value);
     }
+
+    useEffect(() => {
+      if (textId?.tag == "%txtcast{0}") {
+        const first = actions?.list.filter((e: Types.TAction) => e.id >= 400 && e.id < 600)[0];
+        if (!first) return;
+        onChange(first.id.toString());
+      } else if (textId?.tag == "%txtactbefore{0}" || textId?.tag == "%txtactafter{0}") {
+        const first = actions?.list.filter((e: Types.TAction) => e.id >= 600)[0];
+        if (!first) return;
+        onChange(first.id.toString());
+      }
+    }, [textId]);
 
     if (textId?.tag == "%txtallykilled{0}") {
       return (
